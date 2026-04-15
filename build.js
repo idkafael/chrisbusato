f96b66d1773020f3b24fc755eaac31135d3ab53d
idkafael <idkafael@users.noreply.github.com>
2026-04-15 17:38:49 -0300

    Initial commit - ChrisBusato Quiz Project

---
+++ build.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.mkdirSync("dist", { recursive: true });
+fs.copyFileSync("index.html", "dist/index.html");
+
+const dest = path.join(__dirname, "site-destinations.js");
+if (fs.existsSync(dest)) {
+  fs.copyFileSync(dest, path.join(__dirname, "dist", "site-destinations.js"));
+}
+
+var quizzDir = path.join(__dirname, "dist", "quizz");
+fs.mkdirSync(quizzDir, { recursive: true });
+fs.copyFileSync(
+  path.join(__dirname, "dist", "index.html"),
+  path.join(quizzDir, "index.html")
+);
+
+var dashboardDir = path.join(__dirname, "dist", "dashboard");
+fs.mkdirSync(dashboardDir, { recursive: true });
+fs.copyFileSync(
+  path.join(__dirname, "dashboard.html"),
+  path.join(dashboardDir, "index.html")
+);
+
+var valentineDir = path.join(__dirname, "dist", "valentine");
+fs.mkdirSync(valentineDir, { recursive: true });
+fs.copyFileSync(
+  path.join(__dirname, "valentine.html"),
+  path.join(valentineDir, "index.html")
+);
